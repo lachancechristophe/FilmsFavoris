ca108dac1dcb936959a92ee416676b5fc9757f81
Raphael Lavoie <rapolysec@hotmail.com>
2019-03-01 11:16:14 -0500

    Ajout d'Index

---
+++ class/view/Index.class.php
@@ -0,0 +1,27 @@
+<?php
+    
+    require ("lib/page.class.php");
+    require("../../dao/lib/Connection.class.php")
+    class Index extends Page
+    {
+
+        
+        public function __construct()
+        {
+            
+            $this->doc = parent::initHTML("Accueil", "");
+            $this->doc .= parent::createLink("../../show_movies.php", "Link to the movies");       
+            $this->doc .= parent::endBal("body");
+            $this->doc .= parent::endBal("html");
+                      
+        }
+        
+    
+        
+    }
+    
+    
+
+
+
+?>
