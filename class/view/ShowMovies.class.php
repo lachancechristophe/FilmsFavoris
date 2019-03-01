db1f5262040b8d64fba9044974f04d5dbeac1ca2
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-03-01 11:25:31 -0500

    bis

--- class/view/ShowMovies.class.php
+++ class/view/ShowMovies.class.php
@@ -1,7 +1,4 @@
 <?php
-require("Page.class.php");
-require("../../dao/lib/Connection.class.php");
-
 class ShowMovies extends Page {
     private $moviesList = [];
     public function __construct(){
