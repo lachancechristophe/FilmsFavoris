3354d9fc5755f9f45ec9007ac03628b4f94c6acb
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2019-03-01 10:14:10 -0500

    functions.php

---
+++ README.md
@@ -0,0 +1,1 @@
+Projet rapide de programmeurs rapides
---
+++ functions.php
@@ -0,0 +1,91 @@
+<?php
+class Page
+{
+    public function initHTML($titre, $css)
+    {
+        $retStr = "<!DOCTYPE html>\n";
+        $retStr .= "<html lang='fr'>\n";
+        $retStr .= "<head>\n";
+        $retStr .= "<title>".$titre."</title>\n";
+        if (!empty($css)) {
+            $retStr .= '<link rel="stylesheet" type="text/css" href="' . $css .'">';
+        }
+        $retStr .= "</head>\n";
+        $retStr .= "<body>\n";
+        return $retStr;
+    }
+
+    public function beginForm($method, $action, $name)
+    {
+        return "<form method='" . $method . "' action='" . $action . "' name='" . $name . "' >\n";
+    }
+
+    public function insertInput($type, $name, $humantext)
+    {
+        $retStr = "<p><label for='" . $name . "'>" . $humantext . "</label><br/>";
+        return $retStr . "<input name='" . $name . "' type='" . $type . "' value='' /> </p>";
+    }
+
+    public function insertHidden($name, $value)
+    {
+        return "<input type='hidden' name='" . $name . "' value='" . $value . "' ></input>";
+    }
+
+    public function createLink($href, $text)
+    {
+        return '<a href="' . $href . '">' . $text . '</a>';
+    }
+
+    public function beginBal($bal)
+    {
+        return "<" . $bal . ">";
+    }
+
+    public function endBal($bal)
+    {
+        return "</" . $bal . ">";
+    }
+
+    public function br()
+    {
+        return "<br/>\n";
+    }
+
+    public function insertHeading()
+    {
+        $retStr = Page::beginEndBal("h1", "Voitures exotiques");
+        $retStr .= Page::beginEndBal("h2", "et leur propriétaire,");
+        $retStr .= Page::beginEndBal("h3", "Une véritable histoire d'amour.");
+        $retStr .= Page::beginEndBal("p", "Découvrez la passion de Christophe, Éric et Enrique Iglesias!");
+        return $retStr;
+    }
+
+    public function beginEndBal($bal, $content)
+    {
+        $retStr = Page::beginBal($bal);
+        $retStr .= $content;
+        $retStr .= Page::endBal($bal);
+        return $retStr . "\n";
+    }
+}
+function connectReturnPDO()
+{
+    $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;",
+                      "192.168.56.10",
+                      '5432',
+                      'testdb');
+    $user = 'master';
+    $pass = '123qweQWE';
+
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        $pdo = new PDO($conStr, $user, $pass, $options);
+        return $pdo;
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}
