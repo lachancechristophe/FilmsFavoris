dc487f4d11ca92429c4020b8c4d09f89f7894a72
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-03-01 11:23:53 -0500

    wtf

--- class/view/lib/Page.class.php
+++ class/view/lib/Page.class.php
@@ -74,24 +74,3 @@ class Page
         return $retStr . "\n";
     }
 }
-function connectReturnPDO()
-{
-    $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;",
-                      "192.168.56.10",
-                      '5432',
-                      'testdb');
-    $user = 'master';
-    $pass = '123qweQWE';
-
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    try {
-        $pdo = new PDO($conStr, $user, $pass, $options);
-        return $pdo;
-    } catch (\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int)$e->getCode());
-    }
-}
