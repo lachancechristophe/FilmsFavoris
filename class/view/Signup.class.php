589f838b9ab66f47fbc61a97852c2b37f3a980d2
polockv1337 <dont0touch@gmail.com>
2019-03-04 17:40:20 -0500

    sign up

--- class/view/Signup.class.php
+++ class/view/Signup.class.php
@@ -3,31 +3,115 @@
 
 class Signup extends Page 
 {
-    public $doc;
+
     
-    public function __construct()
+    function __construct()
     {
-        $this->doc .= '';
-        
-    
+        $this->doc = '';
+        $this->signUpCheck();
+        $this->formSignUp();
+    }
     function formSignUp()
     {
         $this->doc.='<form class="signUp" action="signUP.php" method="POST" >
-            <input type="text" name="first" placeholder="Firstname">
-            <input type="text" name="last" placeholder="Lastname">
-            <input type="text" name="email" placeholder="E-mail">
             <input type="text" name="uid" placeholder="Username">
             <input type="password" name="pwd" placeholder="password">
             <button type="submit" name="submit">Sign up</button>
             <input name= "f_id" type="hidden" value="signup">
-            </form>';
+            </form><a href="index.php">acceuil</a>';
         
         
         
     }
-    function signUpCheck()
+   
+    protected function signUpCheck()
     {
-        
+        if (isset( $_POST['f_id']))
+        {
+            
+
+            $uidpg= pg_escape_string($_REQUEST['uid']);
+            $pwdpg = pg_escape_string($_REQUEST['pwd']);
+
+
+
+
+            
+            $uid = htmlspecialchars($uidpg);
+            $pwd = htmlspecialchars($pwdpg);
+
+
+
+
+
+            //error handler 
+            //check empty fields 
+
+
+            if(false)//strpos($uid, '<') !== false||strpos($a, '>') !== false
+            {
+                header("location:signup.php?signUp=NoInjectionXd");
+
+                    exit();
+
+            }
+            else
+            {
+
+
+                if(empty($uid) ||empty($pwd))
+                {
+
+                    //echo "<script>alert('empty')/script>";
+                    header("location:signup.php?signUp=empty");
+
+                    exit(); 
+                }
+
+                else//valid character check
+                {
+                    
+                    $conn = new Connection();
+                    $pdo = $conn->getPDO();
+
+                    $sql = $pdo->prepare("SELECT * FROM public.user WHERE user_name=:uid");
+                    $sql->bindParam(':uid', $uid);
+                    $sql->execute();
+                    $data = $sql->fetchAll();
+                    $rows = count($data);
+
+
+                    if($rows>0 )
+                    {
+                        header("location:signup.php?signUp=UserUtiliser");
+                        exit(); 
+
+
+                    }
+                    else
+                    {
+                        //hasshing
+                        $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
+
+
+                        $sql= $pdo->prepare("INSERT INTO public.user (user_name, hashed_pwd) VALUES (:uid,:hashedPwd)");
+                        
+                        $sql->bindParam(':uid', $uid);
+                        $sql->bindParam(':hashedPwd', $hashedPwd);
+
+                        $sql->execute();
+
+                        header("location:signup.php?signUp=Sucsess");
+                        exit(); 
+                    }  
+
+                }
+
+                    
+                
+            }
+
+        }
         
     }
 }
--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
-
+require_once('class/dao/lib/Connection.class.php');
 require_once('class/view/lib/Page.class.php');
 require_once('class/view/Signup.class.php');
 
 $signup = new Signup();
-$signup->showPage();
+$signup->showDoc();
