53763aaf52b201ee2cb4cbb164fb97fa5593c268
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-03-04 16:48:42 -0500

    Show movies
    modifications a connection

---
+++ show_movies.php
@@ -0,0 +1,7 @@
+<?php
+require_once('class/dao/lib/Connection.class.php');
+require_once('class/view/lib/Page.class.php');
+require_once('class/view/ShowMovie.class.php');
+
+$showMovie = new ShowMovie();
+$showMovie->showDoc();
