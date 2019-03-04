1247f9fc7b75b8e4d4df5d689d8bdf4ebaab6137
LavoieEricEnseignant <35667927+LavoieEricEnseignant@users.noreply.github.com>
2019-03-04 17:44:27 -0500

    Edit file struct for Movie, User, UserMovie

--- class/dao/record/UserMovie.class.php
+++ class/dao/record/UserMovie.class.php
@@ -3,6 +3,25 @@
 class UserMovie extends Record
 {
 
+    // --------- Static --------- //
+
+    
+    
+    // --------- CONST and properties --------- //
+    
+    
+    // --------- Public services --------- //
+
+    public function __construct($url) 
+    {
+
+    }
+    
+    // --------- Protected services --------- //
+    
+    
+    
+    // --------- Private services --------- //
 
 
 }
