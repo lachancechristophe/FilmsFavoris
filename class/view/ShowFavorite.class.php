38a300c5f2cbf02bcf2de1774fdd522fc028f32a
Raphael Lavoie <rapolysec@hotmail.com>
2019-03-08 10:38:03 -0500

    Show favorite qui pourrait potentiellement fonctionner, dunno

---
+++ class/view/ShowFavorite.class.php
@@ -0,0 +1,34 @@
+<?php
+class ShowFavorite extends Page {
+    private $favoriteList = [];
+    public function __construct(){
+        $conn = new Connection();
+        $pdo = $conn->getPDO();
+        $query = "SELECT * FROM favorite_movie";
+        $favoriteList = $pdo->query($query);
+        $this->createFormatted($favoriteList);
+    }
+
+    private function createFormatted($stmt)
+    {
+        $retStr = parent::beginBal("table");
+        $retStr .=parent::beginBal("tr");
+
+        $retStr .= parent::beginEndBal("td", "ID User");
+        $retStr .= parent::beginEndBal("td", "ID Movie");
+        
+        
+
+        $retStr .= parent::endBal("tr");
+
+        foreach ($stmt as $row) {
+            $retStr .= parent::beginBal("tr");
+
+            $retStr .= parent::beginEndBal("td", $row['user_id']);
+            $retStr .= parent::beginEndBal("td", $row['movie_id']);
+            $retStr .= parent::endBal("tr");
+        }
+        $retStr .= parent::endBal("table");
+        $this->doc .= $retStr;
+    }
+}
