88c9e37cde150ea30514c30979f707365c92f244
Douvry <valentin.jabba@gmail.com>
2019-03-29 11:35:23 -0400

    Supression double favori

--- class/view/ShowMovie.class.php
+++ class/view/ShowMovie.class.php
@@ -24,10 +24,22 @@ class ShowMovie extends Page {
 
             $conn = new Connection();
             $pdo = $conn->getPDO();
-            $query = "INSERT INTO favorite_movie (user_id, movie_id)";
-            $query .= "VALUES ('" . $_SESSION['user_id'] . "', '" . $_REQUEST['movie_id'] . "');";
-            $pdo->query($query);
-            $this->doc .= "vous avez favoriter un film!";
+
+            $queryCheckFav = "SELECT * FROM favorite_movie WHERE movie_id=".$_REQUEST['movie_id']." AND user_id=".$_SESSION['user_id'];
+            $list_fav = $pdo->query($queryCheckFav);
+            $row = $list_fav->fetch();
+
+            if($row > 0){
+                $this->doc .= "Film deja Favori !";
+            }
+            else{
+                $query = "INSERT INTO favorite_movie (user_id, movie_id)";
+                $query .= "VALUES ('" . $_SESSION['user_id'] . "', '" . $_REQUEST['movie_id'] . "');";
+                $pdo->query($query);
+                $this->doc .= "vous avez favoriter un film!";
+            }
+            
+            
         
         }
         
@@ -51,6 +63,8 @@ class ShowMovie extends Page {
         
 
         foreach ($stmt as $row) {
+
+            $isFav=$this->checkIfFav($row['id']);
             $retStr .= parent::beginBal("tr");
             $lienDetail = 'detail_movie.php?movie_id='.$row['id'].'';
             $retStr .= parent::beginEndBal("td",$row['id']);
@@ -68,12 +82,32 @@ class ShowMovie extends Page {
             $retStr .= parent::beginEndBal("td", $row['producer']);
             $retStr .= parent::beginEndBal("td", $row['release_date']);
             $lienFavoriter = "show_movies.php?movie_id=" . $row['id'] . "&favorite=true";
-            $retStr .= parent::beginEndBal("td", parent::createLink($lienFavoriter, 'Make Favorite'));
+            if($isFav){
+                $retStr .= parent::beginEndBal("td", "Deja favori !");
+            }
+            else{
+                $retStr .= parent::beginEndBal("td", parent::createLink($lienFavoriter, 'Make Favorite'));
+            }
+            
 
             $retStr .= parent::endBal("tr");
         }
         $retStr .= parent::endBal("table");
         $this->doc .= $retStr;
+    }
+
+    protected function checkIfFav($movie_id)
+    {
+        $conn = new Connection();
+        $pdo = $conn->getPDO();
+
+        $queryCheckFav = "SELECT * FROM favorite_movie WHERE movie_id=".$movie_id." AND user_id=".$_SESSION['user_id'];
+        $list_fav = $pdo->query($queryCheckFav);
+        $row = $list_fav->fetch();
+
+        if($row > 0){
+            return true;
+        }
     }
 
     
